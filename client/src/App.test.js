cbff77a9a8d236ba9c2ba37a0534ce0b8c2cf71d
Alex Alexander <kewlpack@gmail.com>
2019-09-10 14:09:50 -0600

    adding solution to repo.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-it("renders without crashing", () => {
-  // will need some sort of mocked up test here.
-});
+it('renders without crashing', () => {});
